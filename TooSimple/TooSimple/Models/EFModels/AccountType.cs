fad84e1323f22db0b92fcd0d366da66a71c50f58
Brandon <blempka@wgu.edu>
2021-11-18 18:18:42 -0600

    change some stuff

--- TooSimple/TooSimple/Models/EFModels/AccountType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TooSimple.Models.EFModels
-{
-    public class AccountType
-    {
-        public int AccountTypeId { get; set; }
-        public string AccountTypeName { get; set; }
-    }
-}
